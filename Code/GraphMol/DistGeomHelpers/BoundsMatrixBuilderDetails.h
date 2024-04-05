69a3942590f5d0231b93583536913ee9ba43aefc
Niels Maeder <maedern@student.ethz.ch>
2024-04-05 13:40:48 +0200

    add details header

---
+++ Code/GraphMol/DistGeomHelpers/BoundsMatrixBuilderDetails.h
@@ -0,0 +1,28 @@
+#include <RDGeneral/export.h>
+
+#include <GraphMol/ForceFieldHelpers/UFF/AtomTyper.h>
+#include <GraphMol/ForceFieldHelpers/MMFF/AtomTyper.h>
+#include <DistGeom/BoundsMatrix.h>
+
+#include <utility>
+
+namespace RDKit {
+class ROMol;
+namespace DGeomHelpers {
+namespace Details {
+
+std::pair<bool, UFF::AtomicParamVect> check12UFF(const ROMol &mol);
+
+std::pair<bool, MMFF::MMFFMolProperties> check12MMFF(const ROMol &mol);
+
+double calc12UFFBounds(const ROMol &mol, Bond *const bond,
+                       UFF::AtomicParamVect params, unsigned int begId,
+                       unsigned int endId);
+
+double calc12MMFFBounds(const ROMol &mol, Bond *const bond,
+                        MMFF::MMFFMolProperties params, unsigned int begId,
+                        unsigned int endId);
+
+}  // namespace Details
+}  // namespace DGeomHelpers
+}  // namespace RDKit
